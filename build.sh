ed52cbd786595c27dbdb7af1270c8ccc9d185b8a
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-19 14:01:31 +0100

    update

--- build.sh
+++ build.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 FLUTTER_BRANCH=`grep channel: .metadata | sed 's/  channel: //g'`
-FLUTTER_REVISION=`grep revision: .metadata | sed 's/  revision: //g'`
 
-# Setup flutter
-FLUTTER=`which flutter`
-if [ $? -eq 0 ]
-then
-  # Flutter is installed
-  FLUTTER=`which flutter`
-else
-  # Get flutter
-  git clone https://github.com/flutter/flutter.git
-  FLUTTER=flutter/bin/flutter
+# Get flutter
+git clone https://github.com/flutter/flutter.git
+FLUTTER=flutter/bin/flutter
+
+#Flutter channel cmd
+cmd="${FLUTTER} channel ${FLUTTER_BRANCH}"
+
+# Set channel info and update
+DIR=$($cmd >& /dev/stdout)
+echo "<!-- $DIR -->"
+$FLUTTER config --enable-web
+if [[ $DIR == *"Your branch is behind"* ]]; then
+  echo "Update starting"
+  $FLUTTER upgrade
+  echo "Update finished"
 fi
 
-cd flutter
-git checkout $FLUTTER_BRANCH
-git pull origin $FLUTTER_BRANCH
-git checkout $FLUTTER_REVISION
-cd ..
-
-$FLUTTER config --enable-web
-
-# Setup dart
-DART=`echo $FLUTTER | sed 's/flutter$/cache\/dart-sdk\/bin\/dart/'`
-echo $DART
-
+# Build flutter for web
 $FLUTTER build web --release
